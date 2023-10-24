515fbc3683f49cee2c4dcd0146dee48566efffe0
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-10-24 18:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+
+let delay = document.getElementById("delay");
+
+let display = document.getElementById("output");
+
+
+async function createPromise (text,delay){
+
+   
+      return new Promise ((resolve) => {
+
+         setTimeout(() => {
+            resolve(text)
+         },delay)
+      })
+   
+
+
+}
+
+async function displayMessage(){
+   try{
+
+      const res = await createPromise(text.innerText,delay.innerText);
+
+      display.innerText = res ;
+
+   }
+
+   catch(error){
+    console.log(error);
+   }
+}
+
+displayMessage();
